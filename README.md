75e899b787449ec4ae0528d1c54d419dd1c76642
viqueen <hrehioui@atlassian.com>
2019-09-09 04:10:25 +0200

    [noissue] - updating the project with dependencies and renaming the mojo class

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ You are using Maven as your build tool, but I am pretty sure the skills/ideas ar
 
 I trust the linked resources are enough to get you started on your platform/environment of choice
 
-### Create a new maven plugin project
+### 1. Create a new maven plugin project
 
 Mojo archetype to the rescue
 
@@ -23,4 +23,49 @@ Mojo archetype to the rescue
       -DarchetypeArtifactId=maven-archetype-mojo
 ```
 
+Update your pom file definition with the following properties and dependencies
 
+```xml
+<properties>
+    <maven.version>3.6.0</maven.version>
+</properties>
+<dependencies>
+    <dependency>
+        <groupId>org.apache.maven</groupId>
+        <artifactId>maven-plugin-api</artifactId>
+        <version>${maven.version}</version>
+    </dependency>
+    <dependency>
+        <groupId>org.apache.maven.plugin-tools</groupId> 
+        <artifactId>maven-plugin-annotations</artifactId>
+        <version>${maven.version}</version>
+    </dependency>
+</dependencies>
+```
+
+Update your java class with the following
+
+```java
+@Mojo(name = "jz", defaultPhase = LifecyclePhase.VALIDATE)
+public class JavazoneMojo extends AbstractMojo {
+    // resolves the target directory
+    @Parameter(defaultValue = "${project.build.directory}")
+    private File outputDirectory;
+    
+    // ...
+}
+```
+
+Now build your plugin
+
+```bash
+cd javazone-maven-plugin
+mvn -e clean install
+
+```
+
+And run it, it should create a file `touch.txt` under your target directory
+
+```bash
+mvn javazone.plugin:javazone-maven-plugin:1.0-SNAPSHOT:jz
+```
--- javazone-maven-plugin/src/main/java/javazone/plugin/JavazoneMojo.java
+++ javazone-maven-plugin/src/main/java/javazone/plugin/JavazoneMojo.java
@@ -18,6 +18,9 @@ package javazone.plugin;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -27,36 +30,35 @@ import java.io.IOException;
  * Goal which touches a timestamp file.
  *
  * @goal touch
- * @phase process-sources
+ * @phase validate
  */
-public class MyMojo
-        extends AbstractMojo {
-    /**
-     * Location of the file.
-     *
-     * @parameter expression="${project.build.directory}"
-     * @required
-     */
+@Mojo(name = "jz", defaultPhase = LifecyclePhase.VALIDATE)
+public class JavazoneMojo extends AbstractMojo {
+
+    @Parameter(defaultValue = "${project.build.directory}")
     private File outputDirectory;
 
-    public void execute()
-            throws MojoExecutionException {
+    public void execute() throws MojoExecutionException {
         File f = outputDirectory;
 
+        // create the output directory if it does not exist
         if (!f.exists()) {
             f.mkdirs();
         }
 
+        // create a file touch.txt
         File touch = new File(f, "touch.txt");
 
         FileWriter w = null;
         try {
             w = new FileWriter(touch);
 
+            // and write to it
             w.write("touch.txt");
         } catch (IOException e) {
             throw new MojoExecutionException("Error creating file " + touch, e);
         } finally {
+            // TODO: bleh we can use try with resource for this
             if (w != null) {
                 try {
                     w.close();
